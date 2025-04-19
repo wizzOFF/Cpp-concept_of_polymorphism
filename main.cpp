4ba4942aaab4bb5b6276c92414e03f425760032b
WizzOFF <maher_off@proton.me>
2025-04-19 20:59:48 +0200

    main.cpp Vehicule.hpp: other version with pointer

--- main.cpp
+++ main.cpp
@@ -3,10 +3,12 @@
 #include "Vehicule.hpp"
 
 //void presenter(Vehicule v)  //Présente le véhicule passé en argument
+/*
 void presenter(Vehicule const& v)  //Présente le véhicule passé en argument
 {
     v.affiche();
 }
+*/
 
 int main()
 {
@@ -18,11 +20,19 @@ int main()
     m.affiche();    //Affiche "Ceci est une moto."
     */
 
+    Vehicule *v(0);
+    v = new Voiture;
+    //On crée une Voiture et on met son adresse dans un pointeur de Vehicule
+
+    v->affiche();  //On affiche "Ceci est une voiture."
+
+    /*
     Vehicule v;
     presenter(v);
 
     Moto m;
     presenter(m);
+    */
 
     return 0;
 }
