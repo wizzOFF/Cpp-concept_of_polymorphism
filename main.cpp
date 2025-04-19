d59d29f8c093dccd953416094f95b073e42d25a7
WizzOFF <maher_off@proton.me>
2025-04-19 18:29:25 +0200

    main.cpp: call affiche()

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
 
+#include <iostream>
+#include "Vehicule.hpp"
+
+int main()
+{
+    Vehicule v;
+    v.affiche();    //Affiche "Ceci est un vehicule."
+
+    Moto m;
+    m.affiche();    //Affiche "Ceci est une moto."
+
+    return 0;
+}
