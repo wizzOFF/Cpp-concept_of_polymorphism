557bea7bca8ba18caaa50209ec67d06f4897c797
WizzOFF <maher_off@proton.me>
2025-04-19 21:34:15 +0200

    Vehicule.cpp: add functions

--- Vehicule.cpp
+++ Vehicule.cpp
@@ -1,17 +1,41 @@
 
 #include "Vehicule.hpp"
 
+Vehicule::Vehicule(int prix)
+    :m_prix(prix)
+{}
+
 void Vehicule::affiche() const
 {
-    cout << "Ceci est un vehicule." << endl;
+    //cout << "Ceci est un vehicule." << endl;
+    cout << "Ceci est un vehicule coutant " << m_prix << " euros." << endl;
 }
+
+Vehicule::~Vehicule() //Même si le destructeur ne fait rien, on doit le mettre !
+{}
+
+Voiture::Voiture(int prix, int portes)
+    :Vehicule(prix), m_portes(portes)   
+{}
 
 void Voiture::affiche() const
 {
-    cout << "Ceci est une voiture." << endl;
+    //cout << "Ceci est une voiture." << endl;
+    cout << "Ceci est une voiture avec " << m_portes << " portes et coutant " << m_prix << " euros." << endl;
 }
+
+Voiture::~Voiture()
+{}
+
+Moto::Moto(int prix, double vitesseMax)
+    :Vehicule(prix), m_vitesse(vitesseMax)
+{}
 
 void Moto::affiche() const
 {
-    cout << "Ceci est une moto." << endl;
+    //cout << "Ceci est une moto." << endl;
+    cout << "Ceci est une moto allant a " << m_vitesse << " km/h et coutant " << m_prix << " euros." << endl;
 }
+
+Moto::~Moto()
+{}
