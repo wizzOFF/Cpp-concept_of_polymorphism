7a21fee3e05c234f76302b66ce0bfc18fe81e640
WizzOFF <maher_off@proton.me>
2025-04-19 21:25:04 +0200

    Vehicule.hpp: add constructors and destructors to classes

--- Vehicule.hpp
+++ Vehicule.hpp
@@ -9,6 +9,8 @@ class Vehicule
     public:
     void affiche() const;  //Affiche une description du Vehicule
     //virtual void affiche() const;  //Affiche une description du Vehicule
+    virtual void affiche() const;
+    virtual ~Vehicule();          //Remarquez le 'virtual' ici
 
     protected:
     int m_prix;  //Chaque véhicule a un prix
@@ -19,6 +21,8 @@ class Voiture : public Vehicule //Une Voiture EST UN Vehicule
     public:
     void affiche() const;
     //virtual void affiche() const;
+    virtual void affiche() const;
+    virtual ~Voiture();
 
     private:
     int m_portes;  //Le nombre de portes de la voiture
@@ -29,6 +33,8 @@ class Moto : public Vehicule  //Une Moto EST UN Vehicule
     public:
     void affiche() const;
     //virtual void affiche() const;
+    virtual void affiche() const;
+    virtual ~Moto();
  
     private:
     double m_vitesse;  //La vitesse maximale de la moto
