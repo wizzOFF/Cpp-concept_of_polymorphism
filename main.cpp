c25533f3b6268183b9cb12423c065a6469df6f5c
WizzOFF <maher_off@proton.me>
2025-04-19 18:32:58 +0200

    main.cpp: static link resolution

--- main.cpp
+++ main.cpp
@@ -2,13 +2,26 @@
 #include <iostream>
 #include "Vehicule.hpp"
 
+void presenter(Vehicule v)  //Présente le véhicule passé en argument
+{
+    v.affiche();
+}
+
 int main()
 {
+    /*
     Vehicule v;
     v.affiche();    //Affiche "Ceci est un vehicule."
 
     Moto m;
     m.affiche();    //Affiche "Ceci est une moto."
+    */
+
+    Vehicule v;
+    presenter(v);
+
+    Moto m;
+    presenter(m);
 
     return 0;
 }
