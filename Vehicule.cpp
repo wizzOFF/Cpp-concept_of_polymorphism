d1798936570bd95f4891240b18e92b494a989bed
WizzOFF <maher_off@proton.me>
2025-04-19 18:03:32 +0200

    Vehicule.cpp: define functions

---
+++ Vehicule.cpp
@@ -0,0 +1,17 @@
+
+#include "Vehicule.hpp"
+
+void Vehicule::affiche() const
+{
+    cout << "Ceci est un vehicule." << endl;
+}
+
+void Voiture::affiche() const
+{
+    cout << "Ceci est une voiture." << endl;
+}
+
+void Moto::affiche() const
+{
+    cout << "Ceci est une moto." << endl;
+}
