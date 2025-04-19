404b9b36b9e6a674d7f63e32faefec2db87165ab
WizzOFF <maher_off@proton.me>
2025-04-19 17:57:02 +0200

    Vehicule.hpp: create class

---
+++ Vehicule.hpp
@@ -0,0 +1,32 @@
+
+#ifndef Vehicule_hpp
+#define Vehicule_hpp
+
+#include <iostream>
+
+class Vehicule
+{
+    public:
+    void affiche() const;  //Affiche une description du Vehicule
+
+    protected:
+    int m_prix;  //Chaque véhicule a un prix
+};
+
+class Voiture : public Vehicule //Une Voiture EST UN Vehicule
+{
+    public:
+    void affiche() const;
+
+    private:
+    int m_portes;  //Le nombre de portes de la voiture
+};
+
+class Moto : public Vehicule  //Une Moto EST UN Vehicule
+{
+    public:
+    void affiche() const;
+ 
+    private:
+    double m_vitesse;  //La vitesse maximale de la moto
+};
