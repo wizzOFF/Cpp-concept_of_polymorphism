a7589c8a7165bc9279a7ab05f346f16f2a6ce6fc
WizzOFF <maher_off@proton.me>
2025-04-19 20:19:15 +0200

    Vehicule.hpp: dynamic link resolution > step 1: declare a virtual method

--- Vehicule.hpp
+++ Vehicule.hpp
@@ -7,7 +7,8 @@
 class Vehicule
 {
     public:
-    void affiche() const;  //Affiche une description du Vehicule
+    //void affiche() const;  //Affiche une description du Vehicule
+    virtual void affiche() const;  //Affiche une description du Vehicule
 
     protected:
     int m_prix;  //Chaque véhicule a un prix
@@ -16,7 +17,8 @@ class Vehicule
 class Voiture : public Vehicule //Une Voiture EST UN Vehicule
 {
     public:
-    void affiche() const;
+    //void affiche() const;
+    virtual void affiche() const;
 
     private:
     int m_portes;  //Le nombre de portes de la voiture
@@ -25,7 +27,8 @@ class Voiture : public Vehicule //Une Voiture EST UN Vehicule
 class Moto : public Vehicule  //Une Moto EST UN Vehicule
 {
     public:
-    void affiche() const;
+    //void affiche() const;
+    virtual void affiche() const;
  
     private:
     double m_vitesse;  //La vitesse maximale de la moto
